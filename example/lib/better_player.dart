3c75a7dad2928962f5a7e8468393f565db5e8ea8
Nicholas Nagy <ygannicholas@gmail.com>
2021-04-30 19:27:58 -0400

    initial commit

---
+++ example/lib/better_player.dart
@@ -0,0 +1,29 @@
+import 'package:better_player/better_player.dart';
+import 'package:flutter/material.dart';
+
+class TheBetterPlayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    BetterPlayerController ctl = BetterPlayerController(
+      BetterPlayerConfiguration(
+        aspectRatio: 16 / 9,
+      ),
+      betterPlayerDataSource: BetterPlayerDataSource(
+          BetterPlayerDataSourceType.network,
+          "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4"),
+    );
+    ctl.play();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Example player"),
+      ),
+      body: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: BetterPlayer(
+          controller: ctl,
+        ),
+      ),
+    );
+  }
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,26 @@
+import 'better_player.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> t = [];
+    for (int number = 0; number < 100; number++) {
+      t.add(TheBetterPlayer());
+    }
+
+    return MaterialApp(
+        home: Directionality(
+            textDirection: TextDirection.ltr,
+            child: PageView(
+              children: t,
+            )));
+  }
+}
